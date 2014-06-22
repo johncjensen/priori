0d7dbcb615bb37633919f57dd6533f32f5c72b25
elliotec <elliotecweb@gmail.com>
2014-06-22 14:52:27 -0600

    add model and controller code for orgs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,27 @@
-class Organization < ActiveRecord::Base
+class Organization
+
+  base_uri "http://api.crunchbase.com/v/2/organization"
+  default_params output: :json
+  format :json
+
+  @api_key = Rails.application.secrets.crunchbase_key
+  
+  attr_accessor :name, :short_description, :primary_image
+
+  def initialize(name, short_description, primary_image)
+    self.name = name
+    self.short_description = short_description
+    self.primary_image = primary_image
+  end
+
+  def self.find(organization_permalink)
+    response = get("/#{organization_permalink}?user_key=#{@api_key}")
+
+    if response.success?
+      self.new(response["name"], response["short_description"], response["primary_image"])
+    else
+      raise response.response
+    end
+    
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
-class Product < ActiveRecord::Base
+class Product
+  
+  base_uri "http://api.crunchbase.com/v/2"
+  default_params output: :json
+  format :json
+
+  @api_key = Rails.application.secrets.crunchbase_key
+
 end
